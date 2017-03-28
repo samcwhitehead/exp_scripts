6d04ecd1c2454388ec3a6033b72b583e579aa27f
psilentp <thlindsay1@gmail.com>
2017-03-28 13:15:02 -0700

    fixed some problems with git commit script

--- git_tools/git_tools.py
+++ git_tools/git_tools.py
@@ -9,16 +9,14 @@ def get_SHA_keys(repo_dirs):
 	return git_SHA
 
 def check_git_status(repo_dirs):
-	repo_dirs = [l.rstrip() for l in repo_dirs]
-	status_strings = [p + ':'+ os.popen('git -C %s status'%(p)).read() for p in repo_dirs]
-	for s in status_strings:
-		print s
-	status = [("""Your branch is up-to-date with 'origin/master'.""" in s) for s in status_strings]
 	import numpy as np
-	if np.sum(status):
-		print 'all tracked repositories are up-to-date'
-		return True
-	else:
+	repo_dirs = [l.rstrip() for l in repo_dirs]
+	status_strings = [os.popen('git -C %s status'%(p)).read() for p in repo_dirs]
+	status = np.array([not("Changes not staged for commit:" in s) for s in status_strings])
+	if np.sum(~status):
 		print 'the following repositories contain uncommited edits in tracked files:'
 		print [repo_dirs[i] for i,s in enumerate(status) if not(s)]
 		return False
+	else:	
+		print 'all tracked repositories are up-to-date'
+		return True
