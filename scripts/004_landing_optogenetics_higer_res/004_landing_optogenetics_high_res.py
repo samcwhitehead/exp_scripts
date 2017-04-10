4fc24e7e2534529bd7564392a7d85870ec1dcd85
psilentp <thlindsay1@gmail.com>
2017-04-10 13:01:40 -0700

    fixed experiment and  fly info string

--- scripts/004_landing_optogenetics_higer_res/004_landing_optogenetics_high_res.py
+++ scripts/004_landing_optogenetics_higer_res/004_landing_optogenetics_high_res.py
@@ -48,7 +48,8 @@ The voltages used to control the epi-led in the pilot experiments were:
 to be the best, so I will go with 0.15. to power the blue light. I run the
 chrimson light at 0.5, 1 , 3,and 5V.
 
-The line is S-28 X C-85. Flies are rased on retinal bottels made on 3.24.
+The line is S-28 X C-85. Flies are rased on retinal bottels made on 4.7
+with 1:250 dilution (200uL) of 100mm ATR.
 
 No legs cut off - testing Chrimson Response in same flies as
 previous experiments. Not head fixed.
@@ -59,7 +60,7 @@ pulse_voltages = [ 0.1, 0.5, 1.0, 2.0, 3.0, 5.0]
 epi_level = 0.125
 """
 
-fly_dob = '4.2.2017'
+fly_dob = '4.7.2017'
 genotype_nickname = "S-28 X C-85 39E01-GcAMP,DN106-Chrimson"
 fly_genotype = """ w+; 
 P{y[+t7.7] w[+mC]=13XLexAop2-IVS-GCaMP6f-p10}su(Hw)attP5, P{y[+t7.7] w[+mC]=GMR38H06-lexA}attP40 / P{w[+mC]=BJD115F05-p65ADzpUw}attP40;
