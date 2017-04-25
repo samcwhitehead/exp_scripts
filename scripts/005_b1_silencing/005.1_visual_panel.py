7e566aa4153aee9df0db24f2db4e0db0ed45ca05
psilentp <thlindsay1@gmail.com>
2017-04-25 09:55:20 -0700

    updated DOB andfor kir/+ flies (E)

--- scripts/005_b1_silencing/005.1_visual_panel.py
+++ scripts/005_b1_silencing/005.1_visual_panel.py
@@ -27,9 +27,9 @@ Legs cut off. Not head fixed.
 epi_level = 0.125
 
 """
-fly_dob = '4.21.2017'
-fly_genotype = """w/w ; GMR39E01-LexA/GCaMP6f-LexOp ; MB258C/UAS-kir2.1::gfp"""
-genotype_nickname = "A-kir2.1"
+fly_dob = '4.22.2017'
+fly_genotype = """w/w ; GMR39E01-LexA/GCaMP6f-LexOp ; +/UAS-kir2.1::gfp"""
+genotype_nickname = "E-kir/+"
 
 ############################################################################
 ########################### Script Variables ###############################
