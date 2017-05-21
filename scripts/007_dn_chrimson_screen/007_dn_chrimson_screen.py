2420c86b33a59644e5447b6ca7caf02639633a1d
psilentp <thlindsay1@gmail.com>
2017-05-21 11:42:57 -0700

    launch file for script and added split_lines helper module

--- scripts/007_dn_chrimson_screen/007_dn_chrimson_screen.py
+++ scripts/007_dn_chrimson_screen/007_dn_chrimson_screen.py
@@ -13,19 +13,30 @@ from ledpanels import display_ctrl
 from muscle_imager.srv import SrvRefFrame
 from muscle_imager.srv import SrvRefFrameRequest
 import readline
+import split_lines as sl
 ############################################################################
 ########################### Metadata Information ###########################
 ############################################################################
 
 exp_description = \
 """
-Test
+CsCrimson screen of the collection of DN split lines from Gwenith and Wyatt.
 """
 fly_dob = raw_input('fly DOB:')
-genotype_nickname = raw_input('S-number:')
+s_number = raw_input('S-number:')
+genotype_nickname = s_number + ' X C-85'
 print genotype_nickname
-#fly_genotype = 
+fly_genotype = sl.expand_snum(s_number)
+print fly_genotype
 pulse_voltages = [ 0.1, 0.5, 1.0, 2.0, 3.0, 5.0]
+
+ol_static_pre_s = 3.0
+ol_static_post_s = 3.0
+ol_motion_s = 1.0
+
+cs_stim_pre_s = 3.0
+cs_stim_pulse = 0.1
+cs_stim_post_s = 3.9
 
 ############################################################################
 ########################### Initialize Experiment ##########################
@@ -39,7 +50,7 @@ with open(script_path,'rt') as f:
 #list of all git tracked repositories
 with open(os.path.join(script_dir,'tracked_git_repos.txt')) as f:
     repo_dirs = f.readlines()
-#assert git_tools.check_git_status(repo_dirs)
+assert git_tools.check_git_status(repo_dirs)
 git_SHA = git_tools.get_SHA_keys(repo_dirs)
 
 #############################################################################
@@ -166,14 +177,48 @@ if __name__ == '__main__':
             ctrl.set_function_by_name('Y','default',freq=50)
             ctrl.set_position(x_pos,0)
             exp_pub.publish(state = 'condition=ol_stripe;loop=open;pattern=%s'%(pattern_name))
-            time.sleep(4.0)
+            time.sleep(ol_static_pre_s)
             ctrl.send_gain_bias(gain_x = 0,gain_y = 0,bias_x = 2*49/5.0*direction)
             ctrl.set_mode('xrate=funcx','y=y0+funcy')
             ctrl.start()
-            time.sleep(1.0)
+            time.sleep(ol_motion_s)
             ctrl.stop()
-            time.sleep(4.0)
+            time.sleep(ol_static_pre_s)
             ############################
+            print 'enter closed loop stripe fixation'
+            ctrl.stop()
+            ctrl.set_pattern_by_name('Pattern_bar.mat')
+            ctrl.set_position(np.random.randint(0,96),0)
+            ctrl.set_function_by_name('Y','default',freq=50)
+            ctrl.send_gain_bias(gain_x = gain_x,bias_x = 0.0,gain_y = 0,bias_y=0)
+            ctrl.set_mode('xrate=ch0','yrate=funcy')
+            ctrl.start()
+            ### publish the state
+            exp_pub.publish(state = 'condition=intertrial_closed_loop;gain=%s;ao_level=%s'%(gain_x,0))
+            time.sleep(5)
+
+        def exc_ol_full(pattern_name,direction,x_pos,block_name):
+            import random
+            pattern_index = (1,2,3)
+            pattern_name = pattern_name%(random.choice(pattern_index))
+            gain_x = -90
+            blk_pub.publish(block_name)
+            ############################ 
+            print block_name
+            ctrl.stop()
+            ctrl.set_pattern_by_name(pattern_name)
+            ctrl.set_function_by_name('X','default',freq=50)
+            ctrl.set_function_by_name('Y','default',freq=50)
+            ctrl.set_position(x_pos,0)
+            exp_pub.publish(state = 'condition=ol_full;loop=open;pattern=%s'%(pattern_name))
+            time.sleep(ol_static_pre_s)
+            ctrl.send_gain_bias(gain_x = 0,gain_y = 0,bias_x = 2*49/5.0*direction)
+            ctrl.set_mode('xrate=funcx','y=y0+funcy')
+            ctrl.start()
+            time.sleep(ol_motion_s)
+            ctrl.stop()
+            time.sleep(ol_static_pre_s)
+            ############################ 
             print 'enter closed loop stripe fixation'
             ctrl.stop()
             ctrl.set_pattern_by_name('Pattern_bar.mat')
@@ -195,13 +240,13 @@ if __name__ == '__main__':
             ctrl.stop()
             ctrl.all_on()
             exp_pub.publish(state = 'open_loop;opto;ao_level=%s;pulse_off'%(0))
-            time.sleep(4.9)
+            time.sleep(cs_stim_pre_s)
             exp_pub.publish(state = 'open_loop;opto;ao_level=%s;pulse_on'%(power_level))
             ctrl.set_ao(3,float(power_level))
-            time.sleep(0.1)
+            time.sleep(cs_stim_pulse)
             exp_pub.publish(state = 'open_loop;opto;ao_level=%s;pulse_off'%(0))
             ctrl.set_ao(3,0)
-            time.sleep(5.0)
+            time.sleep(cs_stim_post_s)
             print 'enter closed loop stripe fixation'
             ctrl.set_pattern_by_name('Pattern_bar.mat')
             ctrl.set_position(np.random.randint(0,96),0)
@@ -211,13 +256,12 @@ if __name__ == '__main__':
             ctrl.start()
             ### publish the state
             exp_pub.publish(state = 'condition=intertrial_closed_loop;gain=%s;ao_level=%s'%(gain_x,0))
-            time.sleep(5)
-
+            time.sleep(30)
 
         ############################################################
-        #call get_ref_frame service, this will not get the current
+        #call get_ref_frame service, this will not only get the current
         #user-defined reference frame but also publish the refrence
-        #frame as a message
+        #frame as a message to be logged in rosbag.
         ############################################################
         print(get_ref_frame_left())
         print(get_ref_frame_right())
@@ -229,24 +273,29 @@ if __name__ == '__main__':
                          fly_genotype = fly_genotype,
                          genotype_nickname = genotype_nickname)
         
-        conditions = {'cl_stripe':       (exc_cl,('Pattern_bar%s.mat',('',),'cl_stripe')),
-                      'cl_optomotor':    (exc_cl,('Pattern_full_%s.mat',(1,2,3),'cl_optomotor')),
-                      'cl_translation':  (exc_cl,('Pattern_contract_%s.mat',(1,2,3),'cl_translation')),
-                      'cl_figure':       (exc_cl,('Pattern_fourier_bar_%s.mat',(1,2,3),'cl_figure')),
+        # Opted against these stimuli - in the intrest of time - Employ these
+        # in a different experiment. Instead include a full field yaw control.
+        #'cl_stripe':       (exc_cl,('Pattern_bar%s.mat',('',),'cl_stripe')),
+        #'cl_optomotor':    (exc_cl,('Pattern_full_%s.mat',(1,2,3),'cl_optomotor')),
+        #'cl_translation':  (exc_cl,('Pattern_contract_%s.mat',(1,2,3),'cl_translation')),
+        #'cl_figure':       (exc_cl,('Pattern_fourier_bar_%s.mat',(1,2,3),'cl_figure')),
 
-                      'ol_expand_center':(exc_ol_expand,('Pattern_expand.mat',21,'ol_expand_center')),
+        conditions = {'ol_expand_center':(exc_ol_expand,('Pattern_expand.mat',21,'ol_expand_center')),
                       'ol_expand_left':  (exc_ol_expand,('Pattern_expand.mat',93,'ol_expand_left')),
                       'ol_expand_right': (exc_ol_expand,('Pattern_expand.mat',45,'ol_expand_right')),
 
                       'ol_stripe_left':  (exc_ol_stripe,('Pattern_bar.mat',-1,90,'ol_stripe_left')),
                       'ol_stripe_right': (exc_ol_stripe,('Pattern_bar.mat',1,42,'ol_stripe_right')),
 
+                      'ol_full_left':    (exc_ol_full,('Pattern_full_%s.mat',-1,90,'ol_stripe_left')),
+                      'ol_full_right':   (exc_ol_full,('Pattern_full_%s.mat',1,42,'ol_stripe_right')),
+
                       'cs_stim_power_0': (exc_cs_stim,(pulse_voltages[0],'cs_stim_power_1')),
                       'cs_stim_power_1': (exc_cs_stim,(pulse_voltages[1],'cs_stim_power_2')),
-                      'cs_stim_power_2': (exc_cs_stim,(pulse_voltages[2],'cs_stim_power_3')),
-                      'cs_stim_power_3': (exc_cs_stim,(pulse_voltages[3],'cs_stim_power_4')),
-                      'cs_stim_power_4': (exc_cs_stim,(pulse_voltages[4],'cs_stim_power_5')),
-                      'cs_stim_power_5': (exc_cs_stim,(pulse_voltages[5],'cs_stim_power_6'))}
+                      'cs_stim_power_3': (exc_cs_stim,(pulse_voltages[2],'cs_stim_power_3')),
+                      'cs_stim_power_4': (exc_cs_stim,(pulse_voltages[3],'cs_stim_power_4')),
+                      'cs_stim_power_5': (exc_cs_stim,(pulse_voltages[4],'cs_stim_power_5')),
+                      'cs_stim_power_6': (exc_cs_stim,(pulse_voltages[5],'cs_stim_power_6'))}
 
         #Run experiment
         t0 = time.time()
---
+++ scripts/007_dn_chrimson_screen/split_lines.py
@@ -0,0 +1,23 @@
+split_dict = {'S-28':{'I':'?',
+					  'II':'P{w[+mC]=BJD115F05-p65ADzpUw}attP40',
+					  'III':'P{w[+mC]=GMR48E11-ZpGal4DBDUw}attP2',
+					  'old_name':'DN106',
+					  'new_name':'P10'}}
+
+
+cs_fly =              {'I':'w[+]',
+					   'II':'P{y[+t7.7] w[+mC]=13XLexAop2-IVS-GCaMP6f-p10}su(Hw)attP5, P{y[+t7.7] w[+mC]=GMR39E01-lexA}attP40',
+					   'III': 'P{20XUAS-IVS-CsChrimson.mVenus}attP2'
+}
+
+chrimson_genotypes = dict()
+
+for key,fly1 in split_dict.items():
+	chrimson_genotypes[key] = {'I':fly1['I'] + '/' + cs_fly['I'],
+	                           'II': fly1['II'] + '/' + cs_fly['II'],
+	                           'III': fly1['III'] + '/' + cs_fly['III']}
+
+def expand_snum(snum):
+	fly = chrimson_genotypes[snum]
+	s_string = ';'.join([fly[k] for k in ['I','II','III']])
+	return s_string
