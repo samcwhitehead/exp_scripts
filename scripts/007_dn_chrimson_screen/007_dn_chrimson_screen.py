e0f2f4647949388225326277006aab9c50a6f8ad
psilentp <thlindsay1@gmail.com>
2017-05-21 16:31:52 -0700

    upgraded firmware on controller

--- scripts/007_dn_chrimson_screen/007_dn_chrimson_screen.py
+++ scripts/007_dn_chrimson_screen/007_dn_chrimson_screen.py
@@ -305,6 +305,7 @@ if __name__ == '__main__':
         ctrl.set_pattern_by_name('Pattern_bar.mat')
         ctrl.set_position(np.random.randint(0,96),0)
         ctrl.set_function_by_name('Y','default',freq=50)
+        ctrl.set_function_by_name('X','default',freq=50)
         ctrl.send_gain_bias(gain_x = gain_x,bias_x = 0.0,gain_y = 0,)
         ctrl.set_mode('xrate=ch0','yrate=funcy')
         blk_pub.publish('prestim_stripe_fix')
