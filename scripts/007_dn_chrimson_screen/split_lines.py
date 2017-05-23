8e02d180241ac6d3c30a4cc35f275dbd2d9a05de
psilentp <thlindsay1@gmail.com>
2017-05-22 17:27:30 -0700

    added S-67 to split database

--- scripts/007_dn_chrimson_screen/split_lines.py
+++ scripts/007_dn_chrimson_screen/split_lines.py
@@ -2,7 +2,13 @@ split_dict = {'S-28':{'I':'?',
 					  'II':'P{w[+mC]=BJD115F05-p65ADzpUw}attP40',
 					  'III':'P{w[+mC]=GMR48E11-ZpGal4DBDUw}attP2',
 					  'old_name':'DN106',
-					  'new_name':'P10'}}
+					  'new_name':'P10'},
+			  'S-67':{'I':'?',
+					  'II':'P{w[+mC]=BJD122B03-pBPp65ADZpUw}attP40',
+					  'III':'P{w[+mC]=GMR22D06-pBPZpGdbdUw}attP2',
+					  'old_name':'DN066',
+					  'new_name':'G25'},
+					  }
 
 
 cs_fly =              {'I':'w[+]',
