63aafb8a9e7e9f7fb93b848fc7d78eee3fbe68b4
psilentp <thlindsay1@gmail.com>
2017-08-17 13:10:40 -0700

    canged wrong thing

--- scripts/008_feedback_gain/008_feedback_gain.py
+++ scripts/008_feedback_gain/008_feedback_gain.py
@@ -152,17 +152,17 @@ if __name__ == '__main__':
         conditions = {
                 'cl_full_bias=0': (exc_cl_full,  ('cl_full_bias=0',  -10,  0)),
                 
-                'cl_full_bias=-5': (exc_cl_full,('cl_full_bias=-2', -2,  -5)),
-                'cl_full_bias=5': (exc_cl_full, ('cl_full_bias=2',   2,   5)),
+                'cl_full_bias=-5': (exc_cl_full,('cl_full_bias=-2', -10,  -2)),
+                'cl_full_bias=5': (exc_cl_full, ('cl_full_bias=2',  -10,   2)),
 
-                'cl_full_bias=-10': (exc_cl_full,('cl_full_bias=-4', -4, -10)),
-                'cl_full_bias=10': (exc_cl_full, ('cl_full_bias=4',   4,  10)),
+                'cl_full_bias=-10': (exc_cl_full,('cl_full_bias=-4', -10, -4)),
+                'cl_full_bias=10': (exc_cl_full, ('cl_full_bias=4',  -10,  4)),
 
-                'cl_full_bias=-15': (exc_cl_full,('cl_full_bias=-6', -6, -15)),
-                'cl_full_bias=15': (exc_cl_full, ('cl_full_bias=6',   6,  15)),
+                'cl_full_bias=-15': (exc_cl_full,('cl_full_bias=-6', -10 -6)),
+                'cl_full_bias=15': (exc_cl_full, ('cl_full_bias=6',  -10,  6)),
 
-                'cl_full_bias=-20': (exc_cl_full,('cl_full_bias=-8', -8, -20)),
-                'cl_full_bias=20': (exc_cl_full, ('cl_full_bias=8',  -8,  20))}
+                'cl_full_bias=-20': (exc_cl_full,('cl_full_bias=-8', -10, -8)),
+                'cl_full_bias=20': (exc_cl_full, ('cl_full_bias=8',  -10,  8))}
 
         #Run experiment
         t0 = time.time()
