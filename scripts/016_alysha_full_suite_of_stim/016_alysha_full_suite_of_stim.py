14a445caeaef5a60df17793b2bceecfcfe7e4c56
lyshds <alyshadesouza@gmail.com>
2019-03-05 16:53:35 -0800

    updated gain

--- scripts/016_alysha_full_suite_of_stim/016_alysha_full_suite_of_stim.py
+++ scripts/016_alysha_full_suite_of_stim/016_alysha_full_suite_of_stim.py
@@ -484,7 +484,7 @@ if __name__ == '__main__':
         ctrl.stop()
         ctrl.set_pattern_by_name('Pattern_yaw_1_ADS_mpm.mat')
         ctrl.set_position(np.random.randint(0,96),0)
-        ctrl.send_gain_bias(gain_x = gain_x,bias_x = 0.0,gain_y = 0,)
+        ctrl.send_gain_bias(gain_x = gain_x,bias_x = 0.0,gain_y = 0,)  #
         ctrl.set_mode('xrate=ch0','yrate=funcy')
         blk_pub.publish('pretrial_stripe_fix')
         ctrl.start()
