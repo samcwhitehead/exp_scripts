80ee6c345003c3d361d6e9b6c2dbc72b1b8eb7c4
lyshds <alyshadesouza@gmail.com>
2019-06-18 17:28:13 -0700

    changing stim names

--- scripts/016_alysha_full_suite_of_stim/016_alysha_full_suite_of_stim.py
+++ scripts/016_alysha_full_suite_of_stim/016_alysha_full_suite_of_stim.py
@@ -468,8 +468,8 @@ if __name__ == '__main__':
         #conditions[last_idx +3] = (exc_pitch_down, 'pitch_down, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=0'%( 0,-4),  ( 0,  -4, 0, 0, 0))
 
 
-        conditions[last_idx +2] = (exc_pitch_up,'pitch_up_actually_roll, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=0'%( 4, 0),  ( 4,  0, 0, 0, 0))
-        conditions[last_idx +3] = (exc_pitch_down, 'pitch_down_actually_roll, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=0'%( 4,0),  ( 4,  0, 0, 0, 0))  #neg pi coded in pattern
+        conditions[last_idx +2] = (exc_pitch_up,'pitch_up_actually_roll, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=0'%( 12, 0),  ( 12,  0, 0, 0, 0))
+        conditions[last_idx +3] = (exc_pitch_down, 'pitch_down_actually_roll, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=0'%( 12,0),  ( 12,  0, 0, 0, 0))  #neg pi coded in pattern
         
         #conditions[last_idx +4] = (exc_ol_blocks,'ol_blocks, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=1'%( 0, 0),  ( 0,   0, 0, 0, 1))
         #conditions[last_idx +5] = (exc_ol_stripe,'ol_stripe, g_x=%s, g_y=%s, b_x=0, b_y=0, ch=1'%( 70, 0), ( 70,  0, 0, 0, 1))
@@ -494,7 +494,7 @@ if __name__ == '__main__':
         #Run experiment
         t0 = time.time()
         print 'enter closed loop stripe fixation'
-        gain_x = -12  #-1 #70
+        gain_x = -1  #-1 #70 #12
         ctrl.stop()
         ctrl.set_pattern_by_name('Pattern_bar.mat')
         ctrl.set_position(np.random.randint(0,96),0)
