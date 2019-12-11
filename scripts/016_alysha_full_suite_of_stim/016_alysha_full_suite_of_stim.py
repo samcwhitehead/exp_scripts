bc039cb54c9477d31d11cdc64e0e86d39daa17da
lyshds <alyshadesouza@gmail.com>
2019-12-10 18:22:26 -0800

    changing gain ... again

--- scripts/016_alysha_full_suite_of_stim/016_alysha_full_suite_of_stim.py
+++ scripts/016_alysha_full_suite_of_stim/016_alysha_full_suite_of_stim.py
@@ -303,7 +303,7 @@ if __name__ == '__main__':
             ctrl.set_position(np.random.randint(0,96),0)
             #ctrl.set_position(18,0)
             ctrl.set_mode('xrate=ch0','yrate=funcy')
-            ctrl.send_gain_bias(gain_x =-1, gain_y = 0, bias_x = 0, bias_y = 0)
+            ctrl.send_gain_bias(gain_x =-2, gain_y = 0, bias_x = 0, bias_y = 0)
             exp_pub.publish('condition=test')
             ctrl.start()
             time.sleep(7)   # 7 
@@ -335,7 +335,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 0, gain_y = gain_y, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=baseline')
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3) #2
             ctrl.stop()
             #ctrl.set_position_function_by_name('X','default')
             ctrl.set_pattern_by_name(pattern_name)
@@ -345,7 +345,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 16, gain_y = gain_y, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=test')
             ctrl.start()
-            time.sleep(7)
+            time.sleep(4) #7
             ctrl.stop()
             #ch_pub.publish('set_a30 %s'%(ch))
             #time.sleep(2)
@@ -353,7 +353,7 @@ if __name__ == '__main__':
             #time.sleep(2)
             ctrl.send_gain_bias(gain_x = 0, gain_y = gain_y, bias_x = bias_x, bias_y = bias_y)
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3) #2
 
             ###60 frames per second -- ~6
 
@@ -377,7 +377,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=baseline')
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             ctrl.stop()
             #ctrl.set_position_function_by_name('X','default')
             ctrl.set_pattern_by_name(pattern_name)
@@ -387,7 +387,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 16, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=test')
             ctrl.start()
-            time.sleep(7)
+            time.sleep(4)
             #ch_pub.publish('set_a30 %s'%(ch))
             #time.sleep(2)
             #ch_pub.publish('set_a30 0')
@@ -395,7 +395,7 @@ if __name__ == '__main__':
             ctrl.stop()
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
 
 
         def exc_pitch_up (block_name,gain_x,gain_y,bias_x,bias_y,ch=0):
@@ -415,7 +415,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = 0,bias_y = 0)
             exp_pub.publish('condition=baseline')
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             ctrl.stop()
             #ctrl.set_position_function_by_name('X','default')
             ctrl.set_pattern_by_name(pattern_name)
@@ -425,7 +425,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 16, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=test')
             ctrl.start()
-            time.sleep(7)
+            time.sleep(4)
             #ch_pub.publish('set_a30 %s'%(ch))
             #time.sleep(2)
             #ch_pub.publish('set_a30 0')
@@ -433,7 +433,7 @@ if __name__ == '__main__':
             ctrl.stop()
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             ctrl.start()
-            time.sleep(2)
+            time.sleep(4)
 
             ###60 frames per second -- ~6
 
@@ -456,7 +456,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = 0,bias_y = 0)
             exp_pub.publish('condition=baseline')
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             ctrl.stop()
             #ctrl.set_position_function_by_name('X','default')
             ctrl.set_pattern_by_name(pattern_name)
@@ -466,7 +466,7 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 16, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=test')
             ctrl.start()
-            time.sleep(7)
+            time.sleep(4)
             #ch_pub.publish('set_a30 %s'%(ch))
             #time.sleep(2)
             #ch_pub.publish('set_a30 0')
@@ -474,7 +474,7 @@ if __name__ == '__main__':
             ctrl.stop()
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
 
 
         def exc_roll_counterclockwise (block_name,gain_x,gain_y,bias_x,bias_y,ch=0):
@@ -494,7 +494,7 @@ if __name__ == '__main__':
             #ctrl.send_gain_bias(gain_x = CL_GAIN_X, gain_y = 0, bias_x = 0,bias_y = 0)
             exp_pub.publish('condition=baseline')
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             ctrl.stop()
             #ctrl.set_position_function_by_name('X','default')
             ctrl.set_pattern_by_name(pattern_name)
@@ -504,11 +504,11 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 16, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             exp_pub.publish('condition=test')
             ctrl.start()
-            time.sleep(7)
+            time.sleep(4)
             ctrl.stop()
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             #ch_pub.publish('set_a30 %s'%(ch))
             #time.sleep(2)
             #ch_pub.publish('set_a30 0')
@@ -533,7 +533,7 @@ if __name__ == '__main__':
             #ctrl.send_gain_bias(gain_x = CL_GAIN_X, gain_y = 0, bias_x = 0,bias_y = 0)
             exp_pub.publish('condition=baseline')
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             ctrl.stop()
             #ctrl.set_position_function_by_name('X','default')
             ctrl.set_pattern_by_name(pattern_name)
@@ -543,12 +543,12 @@ if __name__ == '__main__':
             ctrl.send_gain_bias(gain_x = 16, gain_y = 0, bias_x = bias_x, bias_y = bias_y)  # was 12 
             exp_pub.publish('condition=test')
             ctrl.start()
-            time.sleep(7)
+            time.sleep(4)
             ctrl.stop()
             #ch_pub.publish('set_a30 %s'%(ch))
             ctrl.send_gain_bias(gain_x = 0, gain_y = 0, bias_x = bias_x, bias_y = bias_y)
             ctrl.start()
-            time.sleep(2)
+            time.sleep(3)
             #ch_pub.publish('set_a30 0')
             #time.sleep(2)
 
