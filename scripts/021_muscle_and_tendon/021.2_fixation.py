234e62e165a85cbc2ccbbdd969dbbdf128c77b4a
scw97 <samcwhitehead@gmail.com>
2022-12-13 14:46:57 -0800

    testing rostopic naming convention

--- scripts/021_muscle_and_tendon/021.2_fixation.py
+++ scripts/021_muscle_and_tendon/021.2_fixation.py
@@ -124,19 +124,21 @@ if __name__ == '__main__':
         # ----------------------------------------------------------------------
         # save metadata
         try:
-            get_ref_frame_left = rospy.ServiceProxy('/%s_left/LogRefFrame'%(UNMIXER_NAME), SrvRefFrame)
+            get_ref_frame_left = rospy.ServiceProxy('/%s_left/RefFrameServer'%(UNMIXER_NAME), SrvRefFrame)
             print(get_ref_frame_left())
             rospy.logwarn(get_ref_frame_left())
         except (rospy.service.ServiceException, rospy.ROSException), e:
             print 'LEFT camera not in use: %s'%(e)
+            rospy.logwarn('LEFT camera not in use: %s'%(e))
             get_ref_frame_left = lambda *args, **kwargs: None
 
         try:
-            get_ref_frame_right = rospy.ServiceProxy('/%s_right/LogRefFrame'%(UNMIXER_NAME), SrvRefFrame)
+            get_ref_frame_right = rospy.ServiceProxy('/%s_right/RefFrameServer'%(UNMIXER_NAME), SrvRefFrame)
             print(get_ref_frame_right())
             rospy.logwarn(get_ref_frame_right())
         except (rospy.service.ServiceException, rospy.ROSException), e:
             print 'RIGHT camera not in use: %s'%(e)
+            rospy.logwarn('RIGHT camera not in use: %s'%(e))
             get_ref_frame_right = lambda *args, **kwargs: None
 
         metadata =   {'git_SHA':git_SHA,
