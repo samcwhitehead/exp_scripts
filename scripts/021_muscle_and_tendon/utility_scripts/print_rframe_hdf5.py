c56cc15ebb6dc43843d2266e7df8397e2b63d849
scw97 <samcwhitehead@gmail.com>
2022-12-13 13:28:30 -0800

    print rframe over both sides now

--- scripts/021_muscle_and_tendon/utility_scripts/print_rframe_hdf5.py
+++ scripts/021_muscle_and_tendon/utility_scripts/print_rframe_hdf5.py
@@ -3,34 +3,38 @@ Temp script to print rframe hdf5 contents
 
 """
 import os 
+import sys
 import h5py
 import cPickle
 
 FLYDB_PATH = '/media/imager/DataExternal/FlyDB'
-FLIES = [27]
-SIDE = 'left'
+FLIES = None
+SIDES = ['left', 'right']
 GROUP_KEY = 'LogRefFrame'
 NODE_NAME = 'unmixer'  # 'live_viewer'
 SAVE_FLAG = True
 
 def print_rframe(fly_num):
     fly_path = os.path.join(FLYDB_PATH, 'Fly%04d'%(fly_num))
-    rframe_fn = '%s_%s_rframe_fits.hdf5'%(NODE_NAME, SIDE)
     
-    with h5py.File(os.path.join(fly_path, rframe_fn), 'r') as h5f:
-        for key in h5f[GROUP_KEY].keys():
-            print(key, ' = ', h5f[GROUP_KEY][key][()])
-    
-    if SAVE_FLAG:
-        rframe_dict = dict()
+    # loop over sides
+    for side in SIDES:
+        rframe_fn = '%s_%s_rframe_fits.hdf5'%(NODE_NAME, side)
+        
         with h5py.File(os.path.join(fly_path, rframe_fn), 'r') as h5f:
             for key in h5f[GROUP_KEY].keys():
-                rframe_dict[key] = h5f[GROUP_KEY][key][()][0]
+                print(key, ' = ', h5f[GROUP_KEY][key][()])
         
-        save_path = os.path.join(fly_path, 'ca_camera_%s_rframe_fits.cpkl'%(SIDE))
-        with open(save_path, 'wb') as fp:
-            print(save_path)
-            cPickle.dump(rframe_dict, fp)
+        if SAVE_FLAG:
+            rframe_dict = dict()
+            with h5py.File(os.path.join(fly_path, rframe_fn), 'r') as h5f:
+                for key in h5f[GROUP_KEY].keys():
+                    rframe_dict[key] = h5f[GROUP_KEY][key][()][0]
+            
+            save_path = os.path.join(fly_path, 'ca_camera_%s_rframe_fits.cpkl'%(side))
+            with open(save_path, 'wb') as fp:
+                print(save_path)
+                cPickle.dump(rframe_dict, fp)
         
     
 if __name__ == '__main__':
