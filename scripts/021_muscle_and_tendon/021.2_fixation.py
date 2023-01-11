507e96a5ab636aed593747259191798b06e1a9a0
scw97 <samcwhitehead@gmail.com>
2023-01-11 11:34:48 -0800

    updated expt genotype

--- scripts/021_muscle_and_tendon/021.2_fixation.py
+++ scripts/021_muscle_and_tendon/021.2_fixation.py
@@ -40,10 +40,10 @@ In this particular experiment paradigm, looking at flight start and stop
 Testing flies without GCaMP expression in the muscle but hopefully a brighter fluorophore in the tendons
 """
 
-fly_dob = '12.11.2022'
+fly_dob = '01.10.2023'
 
-fly_genotype = """w[1118]/w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
-genotype_nickname = 'G84/RFP'
+fly_genotype = """w[1118] ; +/+ ; 10XUAS-IVS-Syn21-GFP/sr[md710]"""  #  """w[1118]/w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
+genotype_nickname = 'G84/U14'  #  'G84/RFP'
 
 # fly_genotype = """w[1118]/+[HCS] ; +/(GMR39E01-LexA,GCaMP7f-LexOp) ; +/(sr[md710],UAS-tdTom.S)"""
 # genotype_nickname = 'hinge_and_muscle/HCS'
