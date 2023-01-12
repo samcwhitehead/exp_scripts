5a051108ed4c3d3e3e8fbb836c2aa62b49015a1a
scw97 <samcwhitehead@gmail.com>
2023-01-12 15:50:55 -0800

    updated expt genotype

--- scripts/021_muscle_and_tendon/021.2_fixation.py
+++ scripts/021_muscle_and_tendon/021.2_fixation.py
@@ -42,10 +42,13 @@ Testing flies without GCaMP expression in the muscle but hopefully a brighter fl
 
 fly_dob = '01.10.2023'
 
-fly_genotype = """w[1118] ; +/+ ; 10XUAS-IVS-Syn21-GFP/sr[md710]"""  #  """w[1118]/w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
-genotype_nickname = 'G84/U14'  #  'G84/RFP'
+fly_genotype = """LexAop-GCaMP7c/w[1118] ; GMR39E01-LexA/+ ; +/+"""  
+genotype_nickname = 'X47/L18'  
 
-# fly_genotype = """w[1118]/+[HCS] ; +/(GMR39E01-LexA,GCaMP7f-LexOp) ; +/(sr[md710],UAS-tdTom.S)"""
+# fly_genotype = """w[1118] ; +/+ ; 10XUAS-IVS-Syn21-GFP/sr[md710]"""  #  """w[1118]/w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
+# genotype_nickname = 'G84/U14'  #  'G84/RFP'
+
+# fly_genotype = """LexAop-GCaMP7f/+[HCS] ; GMR39E01-LexA/+ ; +/(sr[md710],UAS-tdTom.S)"""
 # genotype_nickname = 'hinge_and_muscle/HCS'
 
 head_fixed = False 
