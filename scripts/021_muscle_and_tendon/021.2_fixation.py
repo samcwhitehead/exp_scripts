da3dd69e94ec50586da7e7b8ff59188933fd5e41
scw97 <samcwhitehead@gmail.com>
2023-01-25 10:59:31 -0800

    updated expt genotype

--- scripts/021_muscle_and_tendon/021.2_fixation.py
+++ scripts/021_muscle_and_tendon/021.2_fixation.py
@@ -37,16 +37,19 @@ Legs cut off; head NOT fixed (for now)
 
 In this particular experiment paradigm, looking at flight start and stop
 
-Testing flies without GCaMP expression in the muscle but hopefully a brighter fluorophore in the tendons
+Want to see if, given updated camera params, we can image tendons in the red channel
 """
 
-fly_dob = '01.15.2023'
+fly_dob = '01.20.2023'
+
+fly_genotype = """w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
+genotype_nickname = 'G84/RFP'  #  'G84/RFP'
 
 #fly_genotype = """LexAop-GCaMP7c/w[1118] ; GMR39E01-LexA/+ ; +/+"""  
 #genotype_nickname = 'X47/L18'  
 
-fly_genotype = """w[1118] ; +/+ ; 10XUAS-IVS-Syn21-GFP/sr[md710]"""  #  """w[1118]/w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
-genotype_nickname = 'G84/U14'  #  'G84/RFP'
+#fly_genotype = """w[1118] ; +/+ ; 10XUAS-IVS-Syn21-GFP/sr[md710]"""  #  """w[1118]/w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
+#genotype_nickname = 'G84/U14'  #  'G84/RFP'
 
 #fly_genotype = """LexAop-GCaMP7f/+[HCS] ; GMR39E01-LexA/+ ; +/(sr[md710],UAS-tdTom.S)"""
 #genotype_nickname = 'hinge_and_muscle/HCS'
