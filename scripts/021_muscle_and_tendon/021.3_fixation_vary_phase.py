60ae5e3878c68ad6e6e94b965a7de8a41d242c91
scw97 <samcwhitehead@gmail.com>
2023-01-26 14:32:03 -0800

    tweak wb_trig_percent values

--- scripts/021_muscle_and_tendon/021.3_fixation_vary_phase.py
+++ scripts/021_muscle_and_tendon/021.3_fixation_vary_phase.py
@@ -85,10 +85,10 @@ UNMIXER_NAME = 'unmixer'  # 'unmixer' or 'live_viewer'
 WB_TRIG_PERCENT_DEFAULT = 50
 
 # list of values to use for wingbeat phase
-REPEAT_NUM = 2  # for now, going to try to do each phase twice
+#REPEAT_NUM = 2  # for now, going to try to do each phase twice
 #PHASE_VALS = np.linspace(0, 100, np.ceil(NUM_REPS/REPEAT_NUM), endpoint=False)
 PHASE_VALS = [25, 33, 50, 66]
-PHASE_LIST = REPEAT_NUM*list(PHASE_VALS)
+PHASE_LIST = np.ceil(NUM_REPS/len(PHASE_VALS))*list(PHASE_VALS)
 PHASE_LIST = np.random.permutation(PHASE_LIST)  # randomize
 # print(PHASE_LIST)
 
