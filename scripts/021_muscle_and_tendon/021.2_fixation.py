e89c3af7f2e8530f1dca841c0780ef9828fb8963
scw97 <samcwhitehead@gmail.com>
2023-04-10 15:28:18 -0700

    updated fly genotype and dob

--- scripts/021_muscle_and_tendon/021.2_fixation.py
+++ scripts/021_muscle_and_tendon/021.2_fixation.py
@@ -39,13 +39,16 @@ In this particular experiment paradigm, looking at flight start and stop
 
 """
 
-fly_dob = '03.05.2023'
+fly_dob = '04.09.2023'
+
+fly_genotype = """w[*] ; ap[md544]/+ ; 20XUAS-jGCaMP7c/+  """
+genotype_nickname = 'U189/ap-GAL4'
 
 #fly_genotype = """LexAop-GCaMP7b/w[1118] ; GMR39E01-LexA/+ ; +/+"""  
 #genotype_nickname = 'X46/L18'  
 
-fly_genotype = """w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
-genotype_nickname = 'G84/RFP'  #  'G84/RFP'
+#fly_genotype = """w[*] ; 10XUAS-IVS-mCD8::RFP/+ ; sr[md710]/+"""
+#genotype_nickname = 'G84/RFP'  #  'G84/RFP'
 
 #fly_genotype = """LexAop-GCaMP7c/w[1118] ; GMR39E01-LexA/+ ; +/+"""  
 #genotype_nickname = 'X47/L18'  
